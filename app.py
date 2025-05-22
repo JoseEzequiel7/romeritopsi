108677732358da20d353df14803037474c60f993
zeplakk <161462663+JoseEzequiel7@users.noreply.github.com>
2025-05-22 09:52:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ app.secret_key = 'turma-1m'
 
 usuarios = {}
 
-produtos = [
-    {'id': 1, 'nome': 'Camiseta', 'preco': 29.90},
-    {'id': 2, 'nome': 'Calça Jeans', 'preco': 89.90},
-    {'id': 3, 'nome': 'Tênis', 'preco': 149.90},
-]
-
 @app.route('/')
 def index():
     nome_usuario = request.cookies.get('usuario')
